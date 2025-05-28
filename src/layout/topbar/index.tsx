8a22c206d019962ea182007cf1d65642ff18a4b0
Pasupathi M <pasupathip396@gmail.com>
2025-05-28 23:20:11 +0530

    animation added

--- src/layout/topbar/index.tsx
+++ src/layout/topbar/index.tsx
@@ -10,9 +10,9 @@ export default function TopBar() {
   const [hoverBell, setHoverBell] = useState<boolean>(false)
   return (
     <div className="flex flex-1 w-full h-full p-2 items-center justify-between">
-      <div className='flex '>
-        <h2 className="text-base text-white font-bold z-40">Hello, Pasupathi</h2>
-        <div className=''><Lottie animationData={fireAnimation} className='h-[15px] w-[15px]'/></div>
+      <div className='flex'>
+        <h2 className="text-base text-white font-bold z-40">Hey, <span className=' bg-gradient-to-r from-gray-500 to-yellow-500 bg-clip-text text-transparent'>Pasupathi</span></h2>
+        <div className='z-50 h-[15px] w-[15px] hover:h-[20px] transition duration-300 ease-in-out'><Lottie animationData={fireAnimation} className='h-full w-full'/></div>
       </div>
       <div>
         <div className={`hover:bg-[${COLOR.secondary_background}] p-4 rounded-full`}>
